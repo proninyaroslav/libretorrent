04bd75ab50255c9ac6341c9a8b4ca3a7e4e46724
proninyaroslav <proninyaroslav@mail.ru>
2018-08-18 13:10:41 +0300

    Make FileTree depth search non-recursive

--- app/src/main/java/org/proninyaroslav/libretorrent/core/utils/FileTreeDepthFirstSearch.java
+++ app/src/main/java/org/proninyaroslav/libretorrent/core/utils/FileTreeDepthFirstSearch.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Yaroslav Pronin <proninyaroslav@mail.ru>
+ * Copyright (C) 2016, 2018 Yaroslav Pronin <proninyaroslav@mail.ru>
  *
  * This file is part of LibreTorrent.
  *
@@ -25,41 +25,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 /*
  * The modification of algorithm depth-first search for bypass BencodeFileTree.
- *
- * TODO: make non-recursive
  */
 
 public class FileTreeDepthFirstSearch<F extends FileTree<F>>
 {
-    private List<F> leaves = new ArrayList<>();
-    private Map<Integer, F> leavesAsMap = new HashMap<>();
-    private int findIndex = -1;
-    private F findResult;
-
     /*
      * Not recommended for use with a large number of nodes
      * due to deterioration of performance, use getLeaves() method
      */
 
-    public F find(F node, int index)
+    public F find(F startNode, int index)
     {
-        if (node == null)
-            return null;
+        F findResult = null;
 
-        if (findIndex == -1)
-            findIndex = index;
+        Stack<F> stack = new Stack<>();
+        stack.push(startNode);
 
-        if (node.getIndex() == findIndex)
-            findResult = node;
-        else
-            for (String n : node.getChildrenName())
-                if (!node.isFile())
-                    find(node.getChild(n), index);
+        while (!stack.empty()) {
+            F node = stack.pop();
+            if (node == null)
+                continue;
 
-        findIndex = -1;
+            if (node.getIndex() == index)
+                findResult = node;
+            else
+                for (String n : startNode.getChildrenName())
+                    if (!node.isFile())
+                        stack.push(node.getChild(n));
+        }
 
         return findResult;
     }
@@ -68,32 +65,48 @@ public class FileTreeDepthFirstSearch<F extends FileTree<F>>
      * Returns the leaf nodes of the tree.
      */
 
-    public List<F> getLeaves(F node)
+    public List<F> getLeaves(F startNode)
     {
-        if (node == null)
-            return null;
+        List<F> leaves = new ArrayList<>();
 
-        if (node.isFile())
-            leaves.add(node);
+        Stack<F> stack = new Stack<>();
+        stack.push(startNode);
 
-        for (F n : node.getChildren())
-            if (!node.isFile())
-                getLeaves(n);
+        while (!stack.empty()) {
+            F node = stack.pop();
+            if (node == null)
+                continue;
+
+            if (node.isFile())
+                leaves.add(node);
+
+            for (F n : startNode.getChildren())
+                if (!n.isFile())
+                    stack.push(n);
+        }
 
         return leaves;
     }
 
-    public Map<Integer, F> getLeavesAsMap(F node)
+    public Map<Integer, F> getLeavesAsMap(F startNode)
     {
-        if (node == null)
-            return null;
+        Map<Integer, F> leavesAsMap = new HashMap<>();
 
-        if (node.isFile())
-            leavesAsMap.put(node.getIndex(), node);
+        Stack<F> stack = new Stack<>();
+        stack.push(startNode);
 
-        for (F n : node.getChildren())
-            if (!node.isFile())
-                getLeavesAsMap(n);
+        while (!stack.empty()) {
+            F node = stack.pop();
+            if (node == null)
+                continue;
+
+            if (node.isFile())
+                leavesAsMap.put(node.getIndex(), node);
+
+            for (F n : startNode.getChildren())
+                if (!n.isFile())
+                    stack.push(n);
+        }
 
         return leavesAsMap;
     }
