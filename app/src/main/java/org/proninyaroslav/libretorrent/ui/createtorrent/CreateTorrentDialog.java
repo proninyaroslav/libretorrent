9e94b7fdc3b47c8a928c1b8947715394d6c0e8a3
proninyaroslav <proninyaroslav@gmail.com>
2020-03-01 12:36:45 +0300

    [Create torrent] Fix import

--- app/src/main/java/org/proninyaroslav/libretorrent/ui/createtorrent/CreateTorrentDialog.java
+++ app/src/main/java/org/proninyaroslav/libretorrent/ui/createtorrent/CreateTorrentDialog.java
@@ -27,6 +27,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
