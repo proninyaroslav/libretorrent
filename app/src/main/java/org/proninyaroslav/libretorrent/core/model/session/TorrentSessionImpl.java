fcbe9995abd5b0216d83738ab6f31f3bdc1bf3df
proninyaroslav <proninyaroslav@gmail.com>
2021-11-30 12:36:11 +0300

    [Session] Enable announce to all trackers and tiers

--- app/src/main/java/org/proninyaroslav/libretorrent/core/model/session/TorrentSessionImpl.java
+++ app/src/main/java/org/proninyaroslav/libretorrent/core/model/session/TorrentSessionImpl.java
@@ -781,6 +781,8 @@ public class TorrentSessionImpl extends SessionManager
         sp.set_bool(settings_pack.bool_types.enable_ip_notifier.swigValue(), false);
         sp.set_int(settings_pack.int_types.stop_tracker_timeout.swigValue(), 0);
         sp.set_int(settings_pack.int_types.alert_queue_size.swigValue(), 5000);
+        sp.set_bool(settings_pack.bool_types.announce_to_all_trackers.swigValue(), true);
+        sp.set_bool(settings_pack.bool_types.announce_to_all_tiers.swigValue(), true);
 
         String versionName = system.getAppVersionName();
         if (versionName != null) {
